0aa728a7588334d42cdbb87c1192eb20bd968432
t-detlefsen <ted32@pitt.edu>
2024-03-05 08:41:39 -0800

    Added YOLO inference

--- scripts/track-fruit.py
+++ scripts/track-fruit.py
@@ -7,6 +7,9 @@ from sensor_msgs.msg import Image
 
 import cv2
 import numpy as np
+from ultralytics import YOLO
+
+MODEL_PATH = '/home/thomasdetlefsen/FruitNinja/src/fruit-ninja-vision/model.pth'
 
 class trackFruit:
     def __init__(self):
@@ -18,11 +21,17 @@ class trackFruit:
         
         self.cv_bridge = CvBridge()
 
+        self.model = YOLO(MODEL_PATH) 
+
     def callback(self, image_msg, depth_msg):
         img = self.cv_bridge.imgmsg_to_cv2(image_msg)
         depth = self.cv_bridge.imgmsg_to_cv2(depth_msg)
 
-        # PERCEPTION LOGIC GOES HERE
+        # Run inference on image
+        result = self.model(img)[0]
+        box = result.boxes[0].xyxy.squeeze()
+
+        # TO-DO: Get location of ball with respect to camera
 
 if __name__ == "__main__":
     track_fruit = trackFruit()
